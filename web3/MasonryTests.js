a83e97b37f1ffa729f2c7a07577b88eeb59c1490
Catalin Angheluta <catalin@cst-dev.com>
2020-03-20 17:35:46 +0200

    [Feat] Some more js web3 tests for the contract

--- web3/MasonryTests.js
+++ web3/MasonryTests.js
@@ -6,27 +6,64 @@ const contractAbi = contractJson.abi;
 var Web3 = require('web3');
 var web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
 var web3 = new Web3(web3Provider);
+const { toHex } = web3.utils;
 //end fucking initializations
 
-var contractAddress = '0xc9f0a80f0E4efea9197B1a9Dca29a0d10393d2f4';
+var contractAddress = '0xa99D10096FaFF5c9bE71154a8EfE23196d85E260';
 const contract = new web3.eth.Contract(contractAbi, contractAddress);
 // contract.defaultAccount = '0xfc1Ee3C66b6a25Dc0c88bA85D2f37496Cd0250eC'; merge o pula
 
-CallMemberStatuses();
+// SendJoinTheCause()
+// CallMemberStatuses();
+SendStealMoney();
+CallGetTotalInfluence();
 
 function CallMemberStatuses(){
     contract.methods.GetMemberInfluence().call({from: '0xfc1Ee3C66b6a25Dc0c88bA85D2f37496Cd0250eC', gas:5000000})
     .then((res) => {
         console.log('Your influence is: ' + res);
     })
+    .catch(console.log)
     contract.methods.GetMemberRank().call({from: '0xfc1Ee3C66b6a25Dc0c88bA85D2f37496Cd0250eC', gas:5000000})
     .then((res) => {
         console.log('Your rank is: ' + res);
     })
+    .catch(console.log)
+}
+
+function CallGetTotalInfluence(){
+    contract.methods.GetTotalInfluence().call({from: '0xfB1aeBC7606Ed73f95D7713d1Af2BA0f9c834999', gas:5000000})
+    .then((res) => {
+        console.log('Your influence is: ' + res);
+    })
+    .catch(console.log)
 }
 
 function SendJoinTheCause(){
-    contract.methods.JoinTheCause().send({from: '0xfc1Ee3C66b6a25Dc0c88bA85D2f37496Cd0250eC', value:2*(10**18), gas:6721974})
+    contract.methods.JoinTheCause().send({from: '0xfc1Ee3C66b6a25Dc0c88bA85D2f37496Cd0250eC', value:2*(10**18), gas:5000000})
+    .then((res) => {
+        console.log(res);
+
+        contract.methods.GetTotalInfluence().call()
+        .then(console.log)
+    })
+    .catch(console.log)
+}
+
+function SendContributeToTheCause(){
+    contract.methods.JoinTheCause().send({from: '0xfc1Ee3C66b6a25Dc0c88bA85D2f37496Cd0250eC', value:2*(10**18), gas:5000000})
+    .then((res) => {
+        console.log(res);
+
+        contract.methods.GetTotalInfluence().call()
+        .then(console.log)
+    })
+    .catch(console.log)
+}
+
+function SendStealMoney(){
+    let desiredValue = 0.5*(10**18);
+    contract.methods.BreakPonziScheme(toHex(desiredValue)).send({from: '0xfB1aeBC7606Ed73f95D7713d1Af2BA0f9c834999', gas:5000000})
     .then((res) => {
         console.log(res);
 
